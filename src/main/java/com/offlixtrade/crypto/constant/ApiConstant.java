4cfeefb4587355d5676690c3e99576e437d023b2
sumanmanna134 <manna.suman134@gmail.com>
2024-10-05 10:27:06 +0530

    Add swagger ui, versioning

---
+++ src/main/java/com/offlixtrade/crypto/constant/ApiConstant.java
@@ -0,0 +1,8 @@
+package com.offlixtrade.crypto.constant;
+
+public interface ApiConstant {
+
+    static final String API_VERSION = "v1/";
+    public static final String API_PATH = "/api/"+API_VERSION;
+
+}
---
+++ src/main/java/com/offlixtrade/crypto/constant/RouteConstant.java
@@ -0,0 +1,7 @@
+package com.offlixtrade.crypto.constant;
+
+public class RouteConstant implements ApiConstant {
+    public static final String HEALTH = API_PATH + "health";
+}
+
+
